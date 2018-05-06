91358c30f7c4b5e5622a1d77c56b3322bd6915fd
oscarmarinmiro <oscarmarinmiro@github.com>
2018-05-06 21:04:29 +0200

    - new textures and theme data

--- www/js/themes.js
+++ www/js/themes.js
@@ -1,27 +1,44 @@
 DATAVERSE.themes =
 {
     'dark': {
-        'color_scale': 'category20',
-        'default_color': 'black',
+        'color_scale': 'category20b',
+        'default_color': 'grey',
         'cursor_color': 'red',
         'floor': 'yellow',
         'sky': 'examples/world100/media/sky_one.jpg',
         'icon_path': 'themes/dark/icons',
-        'text_color': 'white',
-        'text_background': 'black',
+        'text_color': 'yellow',
+        'text_background': 'green',
 //https://aframe.io/docs/0.8.0/components/text.html
-        'text_font': 'roboto'
+        'text_font': 'monoid',
+        'map_provider': 'CartoDB.DarkMatter',
+        'earth_texture': 'themes/dark/textures/8081-earthmap10k.jpg',
+        'timeline_color': '#000',
+        'panel_color': "white",
+        'panel_aux_color': "black",
+        'panel_font': "roboto",
+        'panel_background': "black",
+        'panel_backpanel': "white"
+
     },
     'light': {
         'color_scale': 'category10',
-        'default_color': 'grey',
+        'default_color': 'purple',
         'cursor_color': 'purple',
         'floor': '#AAA',
         'sky': 'green',
         'icon_path': 'themes/light/icons',
         'text_color': 'black',
         'text_background': 'white',
-        'text_font': 'mozillavr'
+        'text_font': 'kelsonsans',
+        'map_provider': 'CartoDB.Positron',
+        'earth_texture': 'themes/light/textures/8081-earthmap10k.jpg',
+        'timeline_color': '#BFF',
+        'panel_color': "black",
+        'panel_aux_color': "white",
+        'panel_font': "dejavu",
+        'panel_background': "white",
+        'panel_backpanel': "black"
 
     }
 };
